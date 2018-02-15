fb2e9cebf95c79060480c7e2ee6fd0ab3c97f4dc
Cash Myers <cmyers880@gmail.com>
2018-02-15 13:27:27 -0600

    Add/Update potentially missing node-modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { LocationsComponent } from './locations/locations.component';
 import { HeaderComponent } from './header/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LocationsComponent } from './locations/locations.component';
 import { TeachersComponent } from './teachers/teachers.component';
 import { StudentsComponent } from './students/students.component';
 
@@ -14,9 +14,9 @@ import { StudentsComponent } from './students/students.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LocationsComponent,
     HeaderComponent,
     DashboardComponent,
+    LocationsComponent,
     TeachersComponent,
     StudentsComponent
   ],
