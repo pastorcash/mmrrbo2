41f231e11b6ff608496bcd891a1878b477c166b7
Cash Myers <cmyers880@gmail.com>
2018-02-15 13:06:21 -0600

    Added components for header/nav, dashboard, locations, students, and teachers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { LocationsComponent } from './locations/locations.component';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TeachersComponent } from './teachers/teachers.component';
+import { StudentsComponent } from './students/students.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocationsComponent,
+    HeaderComponent,
+    DashboardComponent,
+    TeachersComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule,
