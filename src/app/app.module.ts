82c280f32a3380efa5a0e4fbf41d6d56c3f5c291
Cash Myers <cmyers880@gmail.com>
2018-02-15 13:33:31 -0600

    Added list components for students, teachers and locations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { LocationsComponent } from './locations/locations.component';
 import { TeachersComponent } from './teachers/teachers.component';
 import { StudentsComponent } from './students/students.component';
+import { LocationListComponent } from './locations/location-list/location-list.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { StudentListComponent } from './students/student-list/student-list.component';
+import { TeacherListComponent } from './teachers/teacher-list/teacher-list.component';
 
 
 @NgModule({
@@ -18,7 +22,11 @@ import { StudentsComponent } from './students/students.component';
     DashboardComponent,
     LocationsComponent,
     TeachersComponent,
-    StudentsComponent
+    StudentsComponent,
+    LocationListComponent,
+    ContactsComponent,
+    StudentListComponent,
+    TeacherListComponent
   ],
   imports: [
     BrowserModule,
